0d9f51c9b9e1ecf7545cacc39c11087152f49045
Riyana Gueco <30712653+rngueco@users.noreply.github.com>
2017-09-25 11:28:02 +0800

    UI Prototype (Initial Files)

---
+++ summary.php
@@ -0,0 +1,83 @@
+<html>
+<head>
+	<title>Luke Foundation, Inc</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="header">
+		<div class="handle">
+			<h1>Luke Foundation, Inc</h1>
+		</div>
+		<div class="navi">
+			<h1><a href="index.php" class="active">View Records</a>|
+			<a href="search.php">Search Records</a></h1>
+		</div>
+	</div>
+	<div class="content">
+		<center>
+		<h1>Juan dela Cruz</h1>
+		<a href=""><button type="button" id="simple">View Patient Profile</button></a>
+		<br /><br />
+		<table>
+			<tr>
+				<th colspan="2">Details</th>
+			</tr>
+			<tr>
+				<td><b>Type of cleft lip</b></td>
+				<td>Complete bilateral cleft lip</td>
+			</tr>
+			<tr>
+				<td><b>Type of cleft palate</b></td>
+				<td>Incomplete unilateral cleft palate - right</td>
+			</tr>
+			<tr>
+				<td><b>Type of operation</b></td>
+				<td>Lorem ipsum dolor sit amet.</td>
+			</tr>
+			<tr>
+				<td><b>Type of repair</b></td>
+				<td>Sed id sodales nulla rutrum augue.</td>
+			</tr>
+			<tr>
+				<td><b>Surgeons</b></td>
+				<td>Dr. Who</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>Dr. Strange</td>
+			</tr>
+		</table>
+		<br />
+		<table>
+			<tr>
+				<th colspan="2">Fees</th>
+			</tr>
+			<tr>
+				<td><b>Hospital</b></td>
+				<td>P1250.00</td>
+			</tr>
+			<tr>
+				<td><b>Laboratory</b></td>
+				<td>P400.00</td>
+			</tr>
+		</table><br />
+		<table>
+			<tr>
+				<th>Evaluation Date</th>
+				<th>Admission Date</th>
+				<th>Surgery Date</th>
+				<th>Discharge Date</th>
+				<th>Follow-Up Date</th>
+			</tr>
+			<tr>
+				<td>March 1, 2017</td>
+				<td>March 31, 2017</td>
+				<td>May 5, 2017</td>
+				<td>May 8, 2017</td>
+				<td>June 30, 2017</td>
+			</tr>
+		</table>
+	</center>
+	</div>
+</body>
+</html>
