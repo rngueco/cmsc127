2d139a305142f3efc4f8d796030c183dee38d0fe
Christian <30798095+ChristianNyll@users.noreply.github.com>
2017-10-10 01:39:58 +0800

    Update add_newrec.php

--- add_newrec.php
+++ add_newrec.php
@@ -1,15 +1,17 @@
+<html>
+<body>
 <?php
 if($_POST){
 	include 'link.php';
-	$con = mysql_connect($link);
-	mysql_select_db("root", $con);
 	$fname = $_POST["fname"];
 	$lname = $_POST["lname"];
-	$minital = $_POST["minital"];
-	$query = "INSERT INTO PATIENT (patient_fname, patient_lname, patient_minital) VALUES('$fname', '$lname', '$minital')"
+	$minital = $_POST["minitial"];
+	$query = "INSERT INTO PATIENT (patient_fname, patient_lname, patient_minital) VALUES('$fname', '$lname', '$minital');";
 	$result = mysqli_query($link, $query);
-	$open = mysqli_free_result($result);
-
+	$query = "SELECT patient.patient_id, patient.patient_fname, patient.patient_lname, surgery.Operation, surgery.Repair, surgery.Surgeon FROM patient RIGHT JOIN surgery ON patient.patient_id = surgery.patient_id ORDER BY patient.patient_lname";
+	$result = mysqli_query($link, $query);	
+	$open = mysqli_fetch_array($result);
+	
 	$patient_id = $open['patient_id'];
 	$clip = $_POST["clip"];
 	$cpalate = $_POST["cpalate"];
@@ -21,10 +23,13 @@ if($_POST){
 	$surgdate = $_POST["suryear"]."-".$_POST["surmonth"]."-".$_POST["surday"];
 	$disdate = $_POST["disyear"]."-".$_POST["dismonth"]."-".$_POST["disday"];
 	$fupdate =$_POST["fupyear"]."-".$_POST["fupmonth"]."-".$_POST["fupday"];
-	$hbill = $_POST["hbill"];
-	$lfee = $_POST["lfee"];
+	$hbill = $_POST["hbills"];
+	$lfee = $_POST["lfees"];
 	$query = "INSERT INTO SURGERY(patient_id, Cl_Lip, Cl_Palate, Operation, Repair, Surgeon, Evaluation_Date, Admission_Date, Surgery_Date, Discharge_Date, Follow_Up_Date, Hospital_Bill, Lab_fee)
 	VALUES($patient_id, '$clip', '$cpalate', '$top', '$trep', '$sur', '$edate', '$addate', '$surgdate', '$disdate','$fupdate','$hbill','$lfee');";
 	mysqli_query($link, $query);
+
 }
 ?>
+</body>
+</html>
