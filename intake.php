dbe8beddc9f545faadccd5813ec38081ad2891b1
mtnolasco <31950502+mtnolasco@users.noreply.github.com>
2017-10-26 00:17:32 +0800

    Update intake.php

--- intake.php
+++ intake.php
@@ -1,12 +1,10 @@
 <html>
-
 <head>
 	<title>Luke Foundation, Inc</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script src="script/backToIndex.js" type="text/javascript" ></script>
 	<link href="images/favi.png" type="image/png" rel="icon" />
 </head>
-
 <body>
 	<div class="header">
 		<div class="handle">
@@ -15,7 +13,6 @@
 		</div>
 	</div>
 	<div class="content">
-		
 		<div class="left-panel">
 			<a href="index.php">&nbsp;&nbsp;<img src="images/home.png">&nbsp; Home</a>
 			<a href="view_recs.php">&nbsp;&nbsp;<img src="images/folder.png">&nbsp; View Records</a>
@@ -23,10 +20,6 @@
 			<a href="add_rec.php">&nbsp;&nbsp;<img src="images/add.png">&nbsp; New Record</a>
 			<a href="intake.php">&nbsp;&nbsp;<img src="images/file.png">&nbsp; New Intake Form</a>
 		</div>
-	
-	
-	
-	
 		<div class="right-panel">
 			<center id="toRep">
 				<h1>New Intake Form</h1>
@@ -691,7 +684,7 @@
 						<tr>
 							<td colspan="4" align="center"><input type="submit" value="Submit"></td>
 						</tr>
-					</table>
+					</table><br /><br />
 				</form>
 		
 			</center>
