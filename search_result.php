f6c4cb39867a7e661abd692b9fd2d8b98dcfbc16
Riyana Gueco <rngueco1@up.edu.ph>
2017-10-25 22:34:17 +0800

    Update search_result.php

--- search_result.php
+++ search_result.php
@@ -14,6 +14,15 @@
 		</div>
 	</div>
 	<div class="content">
+		<div class="content">
+		<div class="left-panel">
+			<a href="index.php">&nbsp;&nbsp;<img src="images/home.png">&nbsp; Home</a>
+			<a href="view_recs.php">&nbsp;&nbsp;<img src="images/folder.png">&nbsp; View Records</a>
+			<a href="search.php">&nbsp;&nbsp;<img src="images/search.png">&nbsp; Search Records</a>
+			<a href="add_rec.php">&nbsp;&nbsp;<img src="images/add.png">&nbsp; New Record</a>
+			<a href="intake.php">&nbsp;&nbsp;<img src="images/file.png">&nbsp; New Intake Form</a>
+		</div>
+		<div class="right-panel">
 		<center>
 			<h1>Search Results</h1>
 			<table cellspacing="2px" valign="middle">
@@ -85,6 +94,7 @@
 			?>
 			</table>
 		</center>
+			</div>
 	</div>
 </body>
 </html>
