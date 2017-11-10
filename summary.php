a8b7357209c9ad973c52ace8df965069599d06bf
mtnolasco <31950502+mtnolasco@users.noreply.github.com>
2017-11-10 13:15:56 +0800

    Add files via upload
    may printing na to guys

--- summary.php
+++ summary.php
@@ -1,4 +1,21 @@
 <html>
+<script type="text/javascript">
+
+function printDiv(divName) {
+
+ var printContents = document.getElementById("print-content");
+ var WinPrint = window.open('','','left = 0,top = 0, width = 1000, height = 1100, toolbar = 0, scrollbars = 0, status = 0');
+ WinPrint.document.write('<html><head><link rel="stylesheet" type="text/css" href="css/style.css"></head><body onload = "print(); close();">');
+ WinPrint.document.write(printContents.innerHTML);
+ WinPrint.document.write('</body></html>');
+ WinPrint.document.close();
+ WinPrint.focus();
+ //w=window.open();
+ //w.document.write(printContents);
+ //w.print();
+ //w.close();
+}
+</script>
 <head>
 	<title>Luke Foundation, Inc</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
@@ -17,6 +34,7 @@
 			<?php include 'navigation.php' ?>
 		</div>
 		<div class="right-panel">
+		<div id="print-content">
 		<center>
 			<?php 
 				include 'link.php';
@@ -117,10 +135,14 @@
 					<td>".$fDate."</td>
 				</tr>
 			</table>
+			</div>
+			</center>
 			<br/>
+		<center>	
 			<form action='remove.php' action='post' style='margin-left:10%'>
 				<input type='hidden' name='PID' value='".$open['patient_id']."'><br />
 				<input type='submit' value='Delete Record'>
+				<input type='button' onclick=\"printDiv('print-content')\" value='Print'/>
 			</form>
 		</center>"
 		?>
@@ -128,4 +150,6 @@
 		</div>
 	</div>
 </body>
+
+
 </html>
