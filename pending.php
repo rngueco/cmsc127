9f17454d2540831b211998d420a645652328c1b0
Riyana Gueco <rngueco1@up.edu.ph>
2017-11-13 22:24:08 +0800

    Add files via upload

--- pending.php
+++ pending.php
@@ -86,6 +86,20 @@
                           <input type='image' src='images/view.png' width='17px' height='17px' />
                         </span>
                       </form>
+                      &nbsp;&nbsp;
+                      <form action='approve.php' method='post'>
+                        <input type='hidden' name='iid' value='".$id."'>
+                        <span title='Approve this application'>
+                          <input type='image' src='images/approve.png' width='17px' height='17px' />
+                        </span>
+                      </form>
+                      &nbsp;&nbsp;
+                      <form action='reject.php' method='post'>
+                        <input type='hidden' name='iid' value='".$id."'>
+                        <span title='Reject this application'>
+                          <input type='image' src='images/reject.png' width='17px' height='17px' />
+                        </span>
+                      </form>
                     </td>
                   </tr>
                   ";
