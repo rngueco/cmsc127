492ebd23c984be2092f6e3dbbb894452ce19a255
Paul Jeremiah Naraval <31617826+7NVd@users.noreply.github.com>
2017-11-26 19:54:34 +0800

    Add files via upload

---
+++ bootstrap/search_intake.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Luke Foundation | Cleft Lip and Palate</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php include 'sidebar.php' ?>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <a href="#menu-toggle" class="btn btn-primary" id="menu-toggle">Menu</a>
+                <div class="page-header">
+                  <h1>Search Records <small>Search your records by filter</small></h1>
+                </div>
+                <div class="col-md-10 col-lg-8">
+                  <div class="panel panel-default">
+                    <div class="panel-heading">
+                      <h3 class="panel-title">Choose your filter</h3>
+                    </div>
+                    <div class="panel-body">
+                      <div class="alert alert-info alert-dismissible" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <strong>Tip:</strong> Leave a field blank if you do not intend to use the filter.
+                      </div>
+                      <form action='search_intakeRes.php' method='post'>
+                      <div class="col-lg-12 col-md-12">
+                        <div class="form-group">
+                          <label for="lname">By patient's last name</label>
+                          <input type="text" class="form-control" name="lname" placeholder="Enter last name of patient">
+                        </div>
+                        <div class="form-group">
+                          <label for="fname">By patient's first name</label>
+                          <input type="text" class="form-control" name="fname" placeholder="Enter first name of patient">
+                        </div>
+                      </div>
+                      <div class="col-lg-12 col-md-12">
+                        <center><input type='submit' value='Search' class="btn btn-default"></center>
+                      </div>
+                    </form>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+
+</body>
+
+</html>
---
+++ bootstrap/search_intakeRes.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Luke Foundation | Cleft Lip and Palate</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<?php
+        include 'link.php';
+        $lname = $_REQUEST["lname"];
+        $fname = $_REQUEST["fname"];
+        $query = "SELECT lname, fname, id FROM intake";
+          
+		$conditions = array();
+		if($lname != "") {
+			$conditions[] = "lname='$lname'";
+		}
+		if($fname != "") {
+			$conditions[] = "fname='$fname'";
+		}
+        
+		$sql = $query;
+		if (count($conditions) > 0) {
+			$sql .= " WHERE " . implode(' AND ', $conditions);
+		}
+		$result = mysqli_query($link, $sql);
+		$length = mysqli_num_rows($result);
+?>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php include 'sidebar.php' ?>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <a href="#menu-toggle" class="btn btn-primary" id="menu-toggle">Menu</a>
+                <div class="page-header">
+                  <h1>Search Results <span class="badge"><?=$length?></span></h1>
+                </div>
+                <table class="table-hover" valign="middle">
+                <tr id="h">
+                  <th>Last name</th>
+                  <th>First Name</th>
+                  <th>Edit</th>
+                  <th>View</th>
+                  <th>Accept</th>
+				  <th>Reject</th>
+                </tr>
+                  <?php
+                    while($open = mysqli_fetch_array($result)){
+                      $fname = $open['fname'];
+                      $lname = $open['lname'];
+                      $id = $open['id'];
+                      echo "
+                      <tr>
+                        <td>".$lname."</td>
+                        <td>".$fname."</td>
+                        <td>
+                      <form action='edit_intake.php' method='post'>
+                        <input type='hidden' name='iid' value='".$id."'>
+                        <span title='Edit this record'>
+                          <input type='image' src='images/edit.png' width='17px' height='17px' />
+                        </span>
+                      </form>
+                    </td>
+                    <td>
+                      <form action='intake_sum.php' method='post'>
+                        <input type='hidden' name='iid' value='".$id."'>
+                        <span title='View full summary'>
+                          <input type='image' src='images/view.png' width='17px' height='17px' />
+                        </span>
+                      </form>
+                    </td>
+                    <td>
+                      <form onsubmit='confirm(\"Are you sure you want to approve this patient?\"); approve();'>
+                        <input type='hidden' name='iid' value='".$id."'>
+                        <span title='Approve this application'>
+                          <input type='image' src='images/approve.png' width='17px' height='17px' />
+                        </span>
+                      </form>
+                    </td>
+                    <td>
+                      <form onsubmit='confirm(\"Are you sure you want to reject this patient?\"); reject();'>
+                        <input type='hidden' name='iid' value='".$id."'>
+                        <span title='Reject this application'>
+                          <input type='image' src='images/reject.png' width='17px' height='17px' />
+                        </span>
+                      </form>
+                    </td>
+                      </tr>
+                      ";
+                    }
+                    mysqli_free_result($result);
+                  ?>
+              </table><br />
+              <center><a href="search_intake.php" role="button" class="btn btn-default">Search Again</a></center>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+
+</body>
+
+</html>
