934d6e976b5115ef401e00b77732b34e55b41296
Paul Jeremiah Naraval <31617826+7NVd@users.noreply.github.com>
2017-11-26 23:44:25 +0800

    Add files via upload

--- bootstrap/script/approve.js
+++ bootstrap/script/approve.js
@@ -1,8 +1,5 @@
-function approve(){
+function approve(id){
 	var xhttp = new XMLHttpRequest();
-	var id = document.getElementsByName('iid')[0].value;
-	
-	
 	xhttp.open("POST","approve.php", true);
 	xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 	xhttp.send("iid="+id);
--- bootstrap/script/reject.js
+++ bootstrap/script/reject.js
@@ -1,7 +1,6 @@
-function reject(){
+function reject(id){
 	var xhttp = new XMLHttpRequest();
-	var id = document.getElementsByName('iid')[0].value;
-	xhttp.open("POST","approve.php", true);
+	xhttp.open("POST","reject.php", true);
 	xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 	xhttp.send("iid="+id);
 	var cont = document.getElementById("toRep");
